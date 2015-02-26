fd6432e480110ab4379526480ed3db2dc3669ead
Alexey Belkov <knots@ya.ru>
2015-02-27 01:11:02 +0300

    Add Integer#to_bin, put parser in separate file and rewrite main loop

--- 06/assembler/assembler.rb
+++ 06/assembler/assembler.rb
@@ -1,14 +1,25 @@
 require_relative 'code.rb'
+require_relative 'parser.rb'
 
-# class Parser
-# end
+# monkey-patch Integer to convert numbers to binary
+class Integer
+  def to_bin(width=16)
+    '%0*b' % [width, self]
+  end
+end
 
-asm_lines = File.readlines(ARGV[0])
-hack_lines = asm_lines
-fname, _fext = ARGV[0].split('.')
+parser = Parser.new(ARGV[0])
+filename, _file_extension = ARGV[0].split('.')
 
-File.open("#{fname}.hack", 'w') do |f|
-  hack_lines.each do |line|
-    f.puts(line)
+File.open("#{ filename }.hack", 'w') do |f|
+  parser.parse do |p|
+    if p.command == :a
+      f.puts p.symbol.to_i.to_bin
+    else
+      c = Code.comp(p.comp)
+      d = Code.dest(p.dest)
+      j = Code.jump(p.jump)
+      f.puts "111#{ c }#{ d }#{ j }"
+    end
   end
 end
