7e8cd0052882b0bce6f810195b8821239d245c49
Alexey Belkov <knots@ya.ru>
2015-02-27 03:31:22 +0300

    Build the default symbol_map and do parsing in two passes

--- 06/assembler/assembler.rb
+++ 06/assembler/assembler.rb
@@ -8,9 +8,38 @@ class Integer
   end
 end
 
-parser = Parser.new(ARGV[0])
-filename, _file_extension = ARGV[0].split('.')
+symbol_map = {
+  'SP'     => 0,
+  'LCL'    => 1,
+  'ARG'    => 2,
+  'THIS'   => 3,
+  'THAT'   => 4,
+  'R0'     => 0,
+  'R1'     => 1,
+  'R2'     => 2,
+  'R3'     => 3,
+  'R4'     => 4,
+  'R5'     => 5,
+  'R6'     => 6,
+  'R7'     => 7,
+  'R8'     => 8,
+  'R9'     => 9,
+  'R10'    => 10,
+  'R11'    => 11,
+  'R12'    => 12,
+  'R13'    => 13,
+  'R14'    => 14,
+  'R15'    => 15,
+  'SCREEN' => 16384,
+  'KBD'    => 24576
+}
 
+# 1st pass: update the symbol map with labels and variables
+SymbolParser.new(ARGV[0], symbol_map).parse
+
+# 2nd pass: turn assembly into machine code
+parser = Parser.new(ARGV[0], symbol_map)
+filename, _file_extension = ARGV[0].split('.')
 File.open("#{ filename }.hack", 'w') do |f|
   parser.parse do |p|
     if p.command == :a
