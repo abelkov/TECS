7bf1cd926d401232c5db3148dee2c81a3afb6355
Alexey Belkov <knots@ya.ru>
2015-02-27 15:43:15 +0300

    Require all the files in the assembler.rb

--- 06/assembler/assembler.rb
+++ 06/assembler/assembler.rb
@@ -1,38 +1,10 @@
 require_relative 'code.rb'
-require_relative 'parser.rb'
+require_relative 'integer.rb'
+require_relative 'symbol_map.rb'
+require_relative 'label_parser.rb'
+require_relative 'program_parser.rb'
 
-# monkey-patch Integer to convert numbers to binary
-class Integer
-  def to_bin(width=16)
-    '%0*b' % [width, self]
-  end
-end
-
-symbol_map = {
-  'SP'     => 0,
-  'LCL'    => 1,
-  'ARG'    => 2,
-  'THIS'   => 3,
-  'THAT'   => 4,
-  'R0'     => 0,
-  'R1'     => 1,
-  'R2'     => 2,
-  'R3'     => 3,
-  'R4'     => 4,
-  'R5'     => 5,
-  'R6'     => 6,
-  'R7'     => 7,
-  'R8'     => 8,
-  'R9'     => 9,
-  'R10'    => 10,
-  'R11'    => 11,
-  'R12'    => 12,
-  'R13'    => 13,
-  'R14'    => 14,
-  'R15'    => 15,
-  'SCREEN' => 16384,
-  'KBD'    => 24576
-}
+symbol_map = SymbolMap.symbols
 
 # 1st pass: update the symbol map with labels
 LabelParser.new(ARGV[0], symbol_map).parse
