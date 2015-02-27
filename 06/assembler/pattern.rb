8041ab2c5125b423e5961a77c4b392850e957eeb
Alexey Belkov <knots@ya.ru>
2015-02-27 15:43:03 +0300

    Debug the patterns

--- 06/assembler/pattern.rb
+++ 06/assembler/pattern.rb
@@ -1,26 +1,26 @@
 module Pattern
   def self.at
-    /^\s*@(.+)\s*$/
+    /\s*@(.+)\s*/
   end
 
   def self.at_number
-    /^\s*@(\d+)\s*$/
+    /\s*@(\d+)\s*/
   end
 
   def self.at_symbol
-    /^\s*@(\D+)\s*$/
+    /\s*@([a-zA-Z_.$:]+[0-9a-zA-Z_.$:]*)\s*/
   end
 
   def self.dest
-   /^\s*(.*)=(.*)\s*$/
+   /\s*([D|M|A]*)=([D|M|A|\-|+|&|\||!|0|1]*).*/
   end
 
   def self.jump
-    /^\s*(.*);(.*)\s*$/
+    /\s*(.*);([a-zA-Z]*).*/
   end
 
   def self.label
-    /^\s*\((.*)\)\s*$/
+    /^\s*\((.*)\)\s*/
   end
 
   def self.empty
