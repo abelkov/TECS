230faaaf9cfb45d0fc188c9d458574b05e3d0ad0
Alexey Belkov <knots@ya.ru>
2015-02-27 13:29:49 +0300

    ProgramParser and LabelParser inherit from Parser

--- 06/assembler/parser.rb
+++ 06/assembler/parser.rb
@@ -1,15 +1,14 @@
 class Parser
-  attr_reader :comp, :dest, :jump, :command, :symbol
-
-  def initialize(filename)
+  def initialize(filename, symbol_map)
     @filename = filename
+    @symbol_map = symbol_map
   end
 
   def parse(&block)
     File.readlines(@filename).each do |line|
       next if skip?(line)
       parse_line(line)
-      yield(self)
+      yield(self) if block_given?
     end
   end
 
@@ -18,6 +17,18 @@ class Parser
   def skip?(line)
     line.start_with?('//') || line =~ /^\s*$/
   end
+end
+
+
+class ProgramParser < Parser
+  attr_reader :comp, :dest, :jump, :command, :symbol
+
+  def initialize(filename, symbol_map)
+    super
+    @next_addr = 16 # RAM address
+  end
+
+  private
 
   def parse_line(line)
     at_match = line.match('\s*@(.*)\s*')
@@ -36,6 +47,25 @@ class Parser
       @dest = nil
       @comp = jump_match[1].strip
       @jump = jump_match[2].strip
+    end
+  end
+end
+
+
+class LabelParser < Parser
+  def initialize(filename, symbol_map)
+    super
+    @next_addr = 0 # ROM address
+  end
+
+  private
+
+  def parse_line(line)
+    label_match = line.match('\((.*)\)')
+    if label_match
+      symbol_map[label_match[1]] = @next_addr
+    else
+      @next_addr += 1
     end
   end
 end
