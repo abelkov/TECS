46dba1ce906762eaf720ac2215111b5430ed441c
Alexey Belkov <knots@ya.ru>
2016-06-26 20:27:17 +0300

    asmpattern

--- 06/assembler/src/tecs/assembler/AsmPattern.java
+++ 06/assembler/src/tecs/assembler/AsmPattern.java
@@ -1,7 +1,11 @@
 package tecs.assembler;
 
-/**
- * Created by alexey on 6/25/16.
- */
-public class AsmPattern {
+class AsmPattern {
+    static final String at = "\\s*@(.+)\\s*";
+    static final String atNumber = "\\s*@(\\d+)\\s*";
+    static final String atSymbol ="\\s*@([a-zA-Z_.$:]+[0-9a-zA-Z_.$:]*)\\s*";
+    static final String dest = "\\s*([D|M|A]*)=([D|M|A|\\-|+|&|\\||!|0|1]*).*";
+    static final String jump = "\\s*(.*);([a-zA-Z]*).*";
+    static final String label = "^\\s*\\((.*)\\)\\s*";
+    static final String empty = "^\\s*$";
 }
