dc4e9f29b21a07ee892f947412d3db2d97510d57
Alexey Belkov <knots@ya.ru>
2016-07-02 18:51:09 +0300

    implement patterns

--- 07/src/tecs/vmtranslator/VMPattern.java
+++ 07/src/tecs/vmtranslator/VMPattern.java
@@ -1,15 +1,13 @@
 package tecs.vmtranslator;
 
 class VMPattern {
-	static final String PUSH = "\\s*push\\s+(\\S+)\\s+(\\d+)\\W*";
+	static final String PUSH = "\\s*push\\s+(\\w+)\\s+(\\d+)\\W*";
 	static final String POP = "\\s*pop\\s+(\\S+)\\s+(\\d+)\\W*";
 	static final String ARITHMETIC = "\\s*(add|sub)\\W*";
-
-	// more to come
-	static final String LABEL = "\\s*(add|sub)\\W*";
-	static final String GOTO = "\\s*(add|sub)\\W*";
-	static final String IF = "\\s*(add|sub)\\W*";
-	static final String FUNCTION = "\\s*(add|sub)\\W*";
-	static final String RETURN = "\\s*(add|sub)\\W*";
-	static final String CALL = "\\s*(add|sub)\\W*";
+	static final String LABEL = "\\s*label\\s+(\\w+)\\W*";
+	static final String GOTO = "\\s*goto\\s+(\\w+)\\W*";
+	static final String IF = "\\s*if-goto\\s+(\\w+)\\W*";
+	static final String FUNCTION = "\\s*function\\s+(\\w+)\\s+(\\d+)\\W*";
+	static final String CALL = "\\s*call\\s+(\\w+)\\s+(\\d+)\\W*";
+	static final String RETURN = "\\s*return\\W*";
 }
